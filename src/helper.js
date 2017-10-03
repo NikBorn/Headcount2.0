1d75febf5373a08447bcb270e60b5fc35b7eb041
Nik Bornemeier <NikolausRB@gmail.com>
2017-10-02 21:18:24 -0600

    create constructor in helper file to clean up data and return an object with no duplicate keys.

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,16 @@
 export default class DistrictRepository {
+    constructor(array) {
+        this.data = this.createDataObj(array)
+    }
+
+    createDataObj(array) {
+        return array.reduce((accu, current) => {
+            if(!accu[current.Location]) {
+                accu[current.Location] = {data: {}}
+            }
+            accu[current.Location].data[current.TimeFrame] = current.Data;
+            return accu
+            }, {})
+        }
 
 }
