69bb12f9c05ab3bc0305fda809aa27d3675d60df
Nik Bornemeier <NikolausRB@gmail.com>
2017-10-04 15:55:39 -0600

    finish search functionality and remove graphs from individual cards, it was too expensive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-
 const Header = () => {
-    return(
-        <header className='header'>
-            <h1>Headcount</h1>
-        </header>
-    )
-}
+  return (
+    <header className='header'>
+        <h1>Headcount</h1>
+    </header>
+  );
+};
 
 export default Header;
